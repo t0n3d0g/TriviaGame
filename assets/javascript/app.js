db9ae3a20194a7658925208b3fc070d53715a6f0
t0n3d0g <utexas@excavata.com>
2018-11-07 04:19:21 -0600

    update js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+    // first we initialize the page
+    // wait for user to click "START" button
+    // create "START" button
+    // initialize the game
+    // clear question div
+    // display question with 4 choices
+    // create answer buttons
+    // check response and update score
+    // display timer
+    // end game when no more questions are left
